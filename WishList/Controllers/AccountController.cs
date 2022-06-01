4b92885422bceb4388bda64b88d16f082bb516ba
Sandip Datta <rajasandipdatta@email.com>
2022-06-01 20:23:29 +0530

    2nd commit

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using WishList.Models;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace WishList.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
