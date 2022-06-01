c205fd9d96826056d0c59a5738173ca069279e18
Sandip Datta <rajasandipdatta@email.com>
2022-06-01 20:48:24 +0530

    3rd commit

---
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Email")]
+        public string ConfirmPassword { get; set; }
+    }
+}
